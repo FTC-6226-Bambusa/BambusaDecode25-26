fa54d737af420fef93affb2ce7efef07f2e52b7d
yontuh <jontesteiner@gmail.com>
2026-01-09 19:20:54 -0800

    Pedro got his hands grubby

--- TeamCode/src/main/java/Bambusa/AutoSteps.java
+++
@@ -1,6 +0,0 @@
-package Bambusa;
-
-public class AutoSteps {
-    public static double lookAheadDistance = 10;
-    public static double power = 0.5;
-}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bambusa/ShootingConfig.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode.Bambusa;
+
+public class ShootingConfig {
+    public static String outtake = "outtake";
+    public static String intake = "intake";
+}
